af6c57e19863c0ac5b4883e0c657e8b23a4b7c96
Andre Wruszczak <avataw.dev@gmail.com>
2021-06-17 13:29:24 +0200

    Add cors for client requests

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,22 @@
+import { useEffect, useState } from 'react';
 import { GoldilocksMap } from './GoldilocksMap';
 
 
-function App() {
+export default function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/airport-noise")
+      .then(r => console.log(r));
+  }, [setData])
+
   return (
     <div className="grid grid-cols-2 gap-4">
       <div className="h-screen p-12 ">
+        <pre>
+          {JSON.stringify(data)}
+        </pre>
       </div>
       <div className="h-screen p-6">
         <div className="h-full p-2 border-double border-8 border-black">
@@ -15,5 +27,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
