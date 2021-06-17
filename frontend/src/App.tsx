7aa09b26c3d875523a024062316c313ccdbcf9ba
Andre Wruszczak <avataw.dev@gmail.com>
2021-06-17 12:49:38 +0200

    Modify Layout and add basic styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,8 +4,14 @@ import { GoldilocksMap } from './GoldilocksMap';
 function App() {
   return (
     <div className="grid grid-cols-2 gap-4">
-      <GoldilocksMap />
-      <GoldilocksMap />
+      <div className="h-screen p-12 ">
+      </div>
+      <div className="h-screen p-6">
+        <div className="h-full p-2 border-double border-8 border-black">
+          <GoldilocksMap />
+        </div>
+      </div>
+
     </div>
   );
 }
