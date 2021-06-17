1280d484697823c3f7f1ee5e9d74dd88f3946fe4
Andre Wruszczak <avataw.dev@gmail.com>
2021-06-17 11:40:01 +0200

    Initial commit for Leaflet in React

---
+++ frontend/src/App.tsx
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+
+function App() {
+  return (
+    <div >
+      <MapContainer className="App" center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+         </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+}
+
+export default App;
